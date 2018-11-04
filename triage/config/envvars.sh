4c9a0688865008b7a601a3627bfbf4e914bff76b
Terry Chia <terrycwk1994@gmail.com>
2018-11-04 15:20:20 +0800

    Change reutils $PATH.

--- triage/config/envvars.sh
+++ triage/config/envvars.sh
@@ -2,5 +2,5 @@
 
 tee "/etc/profile.d/myvars.sh" > "/dev/null" <<EOF
 export ATRIAGE_EXPLOITABLE="$HOME/code/exploitable/exploitable/exploitable.py"
-export PATH="/home/vagrant/code/reutils":$PATH
+export PATH="/home/vagrant/code/reutils/bin":$PATH
 EOF
